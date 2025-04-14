bd3ae0ee5bcc7575e5e8f78ae50c71b26df9d691
lewyonq <jakub.lewandowski98@gmail.com>
2025-04-14 16:13:35 +0200

    init commit

---
+++ src/main/java/com/lewyonq/so_cards_app/SoCardsAppApplication.java
@@ -0,0 +1,12 @@
+package com.lewyonq.so_cards_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoCardsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoCardsAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lewyonq/so_cards_app/SoCardsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lewyonq.so_cards_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoCardsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
