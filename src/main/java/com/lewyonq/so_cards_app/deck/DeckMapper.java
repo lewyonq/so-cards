182d44392fad1ac680edc23a0446a732ba12862b
lewyonq <jakub.lewandowski98@gmail.com>
2025-04-19 21:34:12 +0200

    update mapper and fix tests

--- src/main/java/com/lewyonq/so_cards_app/deck/DeckMapper.java
+++ src/main/java/com/lewyonq/so_cards_app/deck/DeckMapper.java
@@ -1,18 +1,18 @@
 package com.lewyonq.so_cards_app.deck;
 
+import com.lewyonq.so_cards_app.card.CardMapper;
 import com.lewyonq.so_cards_app.deck.dto.DeckDetailDto;
 import com.lewyonq.so_cards_app.deck.dto.DeckRequestDto;
 import com.lewyonq.so_cards_app.deck.dto.DeckResponseDto;
 import com.lewyonq.so_cards_app.model.entity.Deck;
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = CardMapper.class)
 public interface DeckMapper {
     Deck toEntity(DeckRequestDto dto);
     DeckResponseDto toResponseDto(Deck deck);
+
+    @Mapping(source = "cards", target = "cardDtos")
     DeckDetailDto toDetailDto(Deck deck);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
--- src/test/java/com/lewyonq/so_cards_app/deck/DeckMapperTest.java
+++ src/test/java/com/lewyonq/so_cards_app/deck/DeckMapperTest.java
@@ -1,12 +1,17 @@
 package com.lewyonq.so_cards_app.deck;
 
+import com.lewyonq.so_cards_app.card.dto.CardBasicDto;
 import com.lewyonq.so_cards_app.deck.dto.DeckDetailDto;
 import com.lewyonq.so_cards_app.deck.dto.DeckRequestDto;
 import com.lewyonq.so_cards_app.deck.dto.DeckResponseDto;
+import com.lewyonq.so_cards_app.model.entity.Card;
 import com.lewyonq.so_cards_app.model.entity.Deck;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -43,9 +48,9 @@ class DeckMapperTest {
         DeckResponseDto dto = deckMapper.toResponseDto(deck);
 
         assertNotNull(dto);
-        assertEquals(dto.getId(), deck.getId());
-        assertEquals(dto.getName(), deck.getName());
-        assertEquals(dto.getDescription(), deck.getDescription());
+        assertEquals(deck.getId(), dto.getId());
+        assertEquals(deck.getName(), dto.getName());
+        assertEquals(deck.getDescription(), dto.getDescription());
     }
 
     @Test
@@ -59,27 +64,72 @@ class DeckMapperTest {
         DeckResponseDto dto = deckMapper.toResponseDto(deck);
 
         assertNotNull(dto);
-        assertEquals(dto.getId(), deck.getId());
-        assertEquals(dto.getName(), deck.getName());
-        assertEquals(dto.getDescription(), deck.getDescription());
+        assertEquals(deck.getId(), dto.getId());
+        assertEquals(deck.getName(), dto.getName());
+        assertEquals(deck.getDescription(), dto.getDescription());
     }
 
     @Test
-    void toDetailDto_Success() {
+    void toDetailDto_Success_WithNoCards() {
         Deck deck = Deck.builder()
                 .id(1L)
                 .name("test name")
                 .description(null)
+                .cards(new ArrayList<>())
                 .build();
 
         DeckDetailDto dto = deckMapper.toDetailDto(deck);
 
         assertNotNull(dto);
-        assertEquals(dto.getId(), deck.getId());
-        assertEquals(dto.getName(), deck.getName());
-        assertEquals(dto.getDescription(), deck.getDescription());
-        assertEquals(dto.getCreatedAt(), deck.getCreatedAt());
-        assertEquals(dto.getUpdatedAt(), deck.getUpdatedAt());
+        assertEquals(deck.getId(), dto.getId());
+        assertEquals(deck.getName(), dto.getName());
+        assertEquals(deck.getDescription(), dto.getDescription());
+        assertEquals(deck.getCreatedAt(), dto.getCreatedAt());
+        assertEquals(deck.getUpdatedAt(), dto.getUpdatedAt());
+        assertEquals(new ArrayList<>(), dto.getCardDtos());
+    }
+
+    @Test
+    void toDetailDto_Success_WithCards() {
+        Card testCard1 = Card.builder()
+                .id(1L)
+                .question("Test question 1")
+                .answer("Test answer 1")
+                .build();
+        Card testCard2 = Card.builder()
+                .id(2L)
+                .question("Test question 2")
+                .answer("Test answer 2")
+                .build();
+        List<Card> cards = List.of(testCard1, testCard2);
+
+        Deck deck = Deck.builder()
+                .id(1L)
+                .name("test name")
+                .description(null)
+                .cards(cards)
+                .build();
+
+        DeckDetailDto dto = deckMapper.toDetailDto(deck);
+
+        assertNotNull(dto);
+        assertEquals(deck.getId(), dto.getId());
+        assertEquals(deck.getName(), dto.getName());
+        assertEquals(deck.getDescription(), dto.getDescription());
+        assertEquals(deck.getCreatedAt(), dto.getCreatedAt());
+        assertEquals(deck.getUpdatedAt(), dto.getUpdatedAt());
+        assertNotNull(dto.getCardDtos());
+        assertEquals(2, dto.getCardDtos().size());
+
+        CardBasicDto actualCardDto1 = dto.getCardDtos().getFirst();
+        assertEquals(testCard1.getId(), actualCardDto1.getId());
+        assertEquals(testCard1.getQuestion(), actualCardDto1.getQuestion());
+        assertEquals(testCard1.getAnswer(), actualCardDto1.getAnswer());
+
+        CardBasicDto actualCardDto2 = dto.getCardDtos().get(1);
+        assertEquals(testCard2.getId(), actualCardDto2.getId());
+        assertEquals(testCard2.getQuestion(), actualCardDto2.getQuestion());
+        assertEquals(testCard2.getAnswer(), actualCardDto2.getAnswer());
     }
 
     @Test
